8004fd1c4c1148db639f6c24e09eedf02338ab5c
Cameron Stanley <cam96stanley@gmail.com>
2026-01-09 22:59:53 -0500

    Implemented contact section

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,40 @@
+import { MdOutlineEmail } from "react-icons/md";
+import { IoLocationSharp } from "react-icons/io5";
+import map from "../../assets/map.jpg";
+
+export default function Contact() {
+  return (
+    <section
+      id="contact"
+      className="py-24 px-4 bg-accent-gradient flex justify-center items-center gap-8 lg:gap-24"
+    >
+      <div>
+        <h2 className="text-3xl font-medium py-4">Get in touch</h2>
+        <p className="w-[300px]">
+          Have a project in mind? I'd love to hear about it.
+        </p>
+        <div className="py-4">
+          <div className="flex items-center gap-2">
+            <MdOutlineEmail />
+            <p className="font-bold">Email</p>
+          </div>
+          <p>cam96stanley@gmail.com</p>
+        </div>
+        <div>
+          <div className="flex items-center gap-2">
+            <IoLocationSharp />
+            <p className="font-bold">Location</p>
+          </div>
+          <p>Charleston SC, USA</p>
+        </div>
+      </div>
+      <div className="hidden md:block">
+        <img
+          src={map}
+          alt="Map of Charleston"
+          className="rounded-3xl shadow-2xl"
+        />
+      </div>
+    </section>
+  );
+}
