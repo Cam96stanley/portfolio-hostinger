3e092849bb1e82f177429dee424cfa1716227a7a
Cameron Stanley <cam96stanley@gmail.com>
2026-01-11 19:55:01 -0500

    added color change on hover for skill logos

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -14,37 +14,37 @@ export default function Skills() {
     >
       <div className="flex justify-center items-center flex-wrap gap-4 text-(--color-accent) lg:gap-10">
         <FaHtml5
-          className="w-12 md:w-14 h-12 md:h-14"
+          className="w-12 md:w-14 h-12 md:h-14 hover:text-(--color-text)"
           aria-label="HTML5"
           title="HTML"
         />
         <FaCss3Alt
-          className="w-12 md:w-14 h-12 md:h-14"
+          className="w-12 md:w-14 h-12 md:h-14 hover:text-(--color-text)"
           aria-label="CSS3"
           title="CSS"
         />
         <FaJs
-          className="w-12 h-12"
+          className="w-12 h-12 hover:text-(--color-text)"
           aria-label="JavaScript"
           title="JavaScript"
         />
         <FaReact
-          className="w-12 md:w-14 h-12 md:h-14"
+          className="w-12 md:w-14 h-12 md:h-14 hover:text-(--color-text)"
           aria-label="ReactJS"
           title="ReactJS"
         />
         <SiNextdotjs
-          className="w-12 md:w-14 h-12 md:h-14"
+          className="w-12 md:w-14 h-12 md:h-14 hover:text-(--color-text)"
           aria-label="NextJS"
           title="NextJS"
         />
         <TbBrandThreejs
-          className="w-12 md:w-14 h-12 md:h-14"
+          className="w-12 md:w-14 h-12 md:h-14 hover:text-(--color-text)"
           aria-label="ThreeJS"
           title="ThreeJS"
         />
         <FaWordpress
-          className="w-12 md:w-14 h-12 md:h-14"
+          className="w-12 md:w-14 h-12 md:h-14 hover:text-(--color-text)"
           aria-label="WordPress"
           title="WordPress"
         />
